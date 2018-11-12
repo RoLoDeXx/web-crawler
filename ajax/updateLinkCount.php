2b68ee667f2ccacd4a5a36318de57e2e485dcca7
RoLoDeXx <35224521+RoLoDeXx@users.noreply.github.com>
2018-11-13 02:12:58 +0530

    Create updateLinkCount.php

---
+++ ajax/updateLinkCount.php
@@ -0,0 +1,11 @@
+<?php
+include("../config.php");
+if(isset($_POST['linkId']))
+  {
+    $query = $con->prepare("UPDATE sites SET clicks = clicks + 1 WHERE id=:id");
+    $query->bindParam("id",$_POST['linkId']);
+    $query->execute();
+  }
+else
+  echo "link pass failed";
+?>
